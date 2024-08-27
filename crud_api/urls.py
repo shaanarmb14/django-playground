ab172c0ec1ddd324b6b775c0e9df4c3927e3ac6b
shaanarmb14 <shaan.arora@mudbath.com.au>
2024-08-27 16:06:50 +1000

    feat(ninja): Start to migrate to django-ninja

--- crud_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # GET many, POST
-    path('',views.index,name='index'), 
-    # Get, Delete, Patch by ID
-    path('<int:movie_id>', views.movie_by_id_handler, name='movie'), 
-]
